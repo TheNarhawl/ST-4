2b779bf32e95982813eba7a21d6ee2cc9d3efa27
Daneelka <danah29122003@gmail.com>
2026-04-24 13:36:26 +0300

    fix bugtest

---
+++ BugPro/Program.cs
@@ -0,0 +1,121 @@
+﻿using Stateless;
+
+namespace BugPro;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var bug = new Bug();
+
+        Console.WriteLine($"Initial: {bug.CurrentState}");
+        bug.StartTriage();
+        Console.WriteLine($"After triage: {bug.CurrentState}");
+        bug.BeginFix();
+        Console.WriteLine($"After begin fix: {bug.CurrentState}");
+        bug.Resolve();
+        Console.WriteLine($"After resolve: {bug.CurrentState}");
+        bug.VerifyOk();
+        Console.WriteLine($"After verify OK: {bug.CurrentState}");
+    }
+}
+
+public sealed class Bug
+{
+    public enum State
+    {
+        New,
+        Triage,
+        Fixing,
+        NeedMoreInfo,
+        Deferred,
+        OtherProduct,
+        ReadyForTest,
+        Returned,
+        Reopened,
+        Closed
+    }
+
+    public enum Trigger
+    {
+        StartTriage,
+        BeginFix,
+        AskMoreInfo,
+        ProvideInfo,
+        Defer,
+        ResumeWork,
+        RouteToOtherProduct,
+        ReturnToOrigin,
+        Resolve,
+        VerifyOk,
+        VerifyFail,
+        Reopen,
+        AnalyzeAgain
+    }
+
+    private readonly StateMachine<State, Trigger> _machine;
+
+    public Bug()
+    {
+        _machine = new StateMachine<State, Trigger>(State.New);
+
+        _machine.Configure(State.New)
+            .Permit(Trigger.StartTriage, State.Triage);
+
+        _machine.Configure(State.Triage)
+            .Permit(Trigger.BeginFix, State.Fixing)
+            .Permit(Trigger.AskMoreInfo, State.NeedMoreInfo)
+            .Permit(Trigger.Defer, State.Deferred)
+            .Permit(Trigger.RouteToOtherProduct, State.OtherProduct)
+            .Permit(Trigger.ReturnToOrigin, State.Returned);
+
+        _machine.Configure(State.NeedMoreInfo)
+            .Permit(Trigger.ProvideInfo, State.Triage)
+            .Permit(Trigger.ReturnToOrigin, State.Returned);
+
+        _machine.Configure(State.Deferred)
+            .Permit(Trigger.ResumeWork, State.Fixing)
+            .Permit(Trigger.ReturnToOrigin, State.Returned);
+
+        _machine.Configure(State.OtherProduct)
+            .Permit(Trigger.ReturnToOrigin, State.Returned);
+
+        _machine.Configure(State.Fixing)
+            .Permit(Trigger.Resolve, State.ReadyForTest)
+            .Permit(Trigger.AskMoreInfo, State.NeedMoreInfo)
+            .Permit(Trigger.Defer, State.Deferred)
+            .Permit(Trigger.RouteToOtherProduct, State.OtherProduct);
+
+        _machine.Configure(State.ReadyForTest)
+            .Permit(Trigger.VerifyOk, State.Closed)
+            .Permit(Trigger.VerifyFail, State.Reopened);
+
+        _machine.Configure(State.Returned)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        _machine.Configure(State.Reopened)
+            .Permit(Trigger.AnalyzeAgain, State.Triage)
+            .Permit(Trigger.ReturnToOrigin, State.Returned);
+
+        _machine.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened);
+    }
+
+    public State CurrentState => _machine.State;
+
+    public IEnumerable<Trigger> PermittedTriggers => _machine.PermittedTriggers;
+
+    public void StartTriage() => _machine.Fire(Trigger.StartTriage);
+    public void BeginFix() => _machine.Fire(Trigger.BeginFix);
+    public void AskMoreInfo() => _machine.Fire(Trigger.AskMoreInfo);
+    public void ProvideInfo() => _machine.Fire(Trigger.ProvideInfo);
+    public void Defer() => _machine.Fire(Trigger.Defer);
+    public void ResumeWork() => _machine.Fire(Trigger.ResumeWork);
+    public void RouteToOtherProduct() => _machine.Fire(Trigger.RouteToOtherProduct);
+    public void ReturnToOrigin() => _machine.Fire(Trigger.ReturnToOrigin);
+    public void Resolve() => _machine.Fire(Trigger.Resolve);
+    public void VerifyOk() => _machine.Fire(Trigger.VerifyOk);
+    public void VerifyFail() => _machine.Fire(Trigger.VerifyFail);
+    public void Reopen() => _machine.Fire(Trigger.Reopen);
+    public void AnalyzeAgain() => _machine.Fire(Trigger.AnalyzeAgain);
+}
---
+++ BugTests/Test1.cs
@@ -0,0 +1,215 @@
+﻿using BugPro;
+using System.Linq;
+
+namespace BugTests;
+
+[TestClass]
+public sealed class BugWorkflowTests
+{
+    [TestMethod]
+    public void InitialState_IsNew()
+    {
+        var bug = new Bug();
+        Assert.AreEqual(Bug.State.New, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void StartTriage_FromNew_GoesToTriage()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        Assert.AreEqual(Bug.State.Triage, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void BeginFix_FromTriage_GoesToFixing()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        Assert.AreEqual(Bug.State.Fixing, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Resolve_FromFixing_GoesToReadyForTest()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        bug.Resolve();
+        Assert.AreEqual(Bug.State.ReadyForTest, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void VerifyOk_FromReadyForTest_GoesToClosed()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        bug.Resolve();
+        bug.VerifyOk();
+        Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void VerifyFail_FromReadyForTest_GoesToReopened()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        bug.Resolve();
+        bug.VerifyFail();
+        Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void AnalyzeAgain_FromReopened_GoesToTriage()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        bug.Resolve();
+        bug.VerifyFail();
+        bug.AnalyzeAgain();
+        Assert.AreEqual(Bug.State.Triage, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ReturnToOrigin_FromTriage_GoesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.ReturnToOrigin();
+        Assert.AreEqual(Bug.State.Returned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Reopen_FromReturned_GoesToReopened()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.ReturnToOrigin();
+        bug.Reopen();
+        Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Reopen_FromClosed_GoesToReopened()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        bug.Resolve();
+        bug.VerifyOk();
+        bug.Reopen();
+        Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void AskMoreInfo_FromTriage_GoesToNeedMoreInfo()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.AskMoreInfo();
+        Assert.AreEqual(Bug.State.NeedMoreInfo, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ProvideInfo_FromNeedMoreInfo_GoesToTriage()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.AskMoreInfo();
+        bug.ProvideInfo();
+        Assert.AreEqual(Bug.State.Triage, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ReturnToOrigin_FromNeedMoreInfo_GoesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.AskMoreInfo();
+        bug.ReturnToOrigin();
+        Assert.AreEqual(Bug.State.Returned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Defer_FromTriage_GoesToDeferred()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.Defer();
+        Assert.AreEqual(Bug.State.Deferred, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ResumeWork_FromDeferred_GoesToFixing()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.Defer();
+        bug.ResumeWork();
+        Assert.AreEqual(Bug.State.Fixing, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Defer_FromFixing_GoesToDeferred()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        bug.Defer();
+        Assert.AreEqual(Bug.State.Deferred, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void RouteToOtherProduct_FromTriage_GoesToOtherProduct()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.RouteToOtherProduct();
+        Assert.AreEqual(Bug.State.OtherProduct, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ReturnToOrigin_FromOtherProduct_GoesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.RouteToOtherProduct();
+        bug.ReturnToOrigin();
+        Assert.AreEqual(Bug.State.Returned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void InvalidTransition_BeginFix_FromNew_Throws()
+    {
+        var bug = new Bug();
+        Assert.ThrowsException<InvalidOperationException>(() => bug.BeginFix());
+    }
+
+    [TestMethod]
+    public void InvalidTransition_VerifyOk_FromFixing_Throws()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.BeginFix();
+        Assert.ThrowsException<InvalidOperationException>(() => bug.VerifyOk());
+    }
+
+    [TestMethod]
+    public void InvalidTransition_ProvideInfo_FromNew_Throws()
+    {
+        var bug = new Bug();
+        Assert.ThrowsException<InvalidOperationException>(() => bug.ProvideInfo());
+    }
+
+    [TestMethod]
+    public void PermittedTriggers_InTriage_ContainsBeginFix()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        Assert.IsTrue(bug.PermittedTriggers.Contains(Bug.Trigger.BeginFix));
+    }
+}
